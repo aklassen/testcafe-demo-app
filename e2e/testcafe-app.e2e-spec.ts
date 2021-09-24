130956979b3a790b02bbceab7132f057c980ad46
André Klaßen <a.klassen@slashwhy.de>
2021-09-24 17:08:41 +0200

    added testcafe

---
+++ e2e/testcafe-app.e2e-spec.ts
@@ -0,0 +1,16 @@
+import { Selector } from 'testcafe';
+    
+fixture('testcafe-demo-app-tests')
+    .page `http://localhost:3000`;
+
+
+test('header is available', async t => {
+    
+    const header = Selector(`[data-testid='header']`);
+    const headerExists = header.exists;
+
+    
+    await t
+        .expect(headerExists).ok()
+    
+});
